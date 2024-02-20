e004b0baa97e26afc62105bb289d21c8a8efcbae
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-20 01:17:06 -0300

    Se bajo el footer

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,8 +1,11 @@
+import { Container } from "react-bootstrap";
+
 const Inicio = () => {
     return (
-        <div>
-            
-        </div>
+        <Container className="flex-grow-1">
+            <h1>Hola</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quis vel maxime sit sint voluptas adipisci, magni voluptatum eius, iure, deleniti facere. Enim impedit corrupti dolorem ullam error consectetur quibusdam.</p>
+        </Container>
     );
 };
 
