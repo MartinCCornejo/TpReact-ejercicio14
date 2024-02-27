fcaf079aa2c03d4097e20bd8e36ee743d058ff02
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-27 01:07:24 -0300

    Se finalizo el diseño del navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,20 +1,21 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 const Menu = () => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+  return (
+    <Navbar expand="md" className="bg-mainColor py-3">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand className="logo"><i className="bi bi-egg-fried me-2"></i>DeliciasVirtuales</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto textPoppins">
+            <Nav.Link className="text-light">Administrador</Nav.Link>
+            <Nav.Link className="text-light">Iniciar Sesión</Nav.Link>
+            <Nav.Link className="text-light">Registrarse</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 };
 
 export default Menu;
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -3,8 +3,9 @@ import { Container } from "react-bootstrap";
 const Inicio = () => {
     return (
         <Container className="flex-grow-1">
-            <h1>Hola</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quis vel maxime sit sint voluptas adipisci, magni voluptatum eius, iure, deleniti facere. Enim impedit corrupti dolorem ullam error consectetur quibusdam.</p>
+            <section className="">
+
+            </section>
         </Container>
     );
 };
