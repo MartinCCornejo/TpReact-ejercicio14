b086cf8e8865e265fc062f075c2da215d17e21e7
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-27 16:39:02 -0300

    Se enlazo la pagina administrador mediante react-router-dom y avanzamos con el maquetado de la pagina

---
+++ src/components/pages/Administrador.jsx
@@ -0,0 +1,41 @@
+import { Container, Table } from "react-bootstrap";
+
+const Administrador = () => {
+  return (
+    <Container className="flex-grow-1">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Cod</th>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>URL de la imagen</th>
+            <th>Categoría</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default Administrador;
