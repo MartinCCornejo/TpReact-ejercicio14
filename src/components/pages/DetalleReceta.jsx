937008b624bbc733fd46a83391c4747647a7bb47
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-28 02:39:37 -0300

    Se maqueto una imagen de la receta con su descripcion detallada

--- src/components/pages/DetalleReceta.jsx
+++ src/components/pages/DetalleReceta.jsx
@@ -1,11 +1,39 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const DetalleReceta = () => {
-    return (
-        <section className="flex-grow-1">
-            <Container className="my-4"></Container>
-        </section>
-    );
+  return (
+    <section className="flex-grow-1 bg-main-color">
+      <Container className="my-4">
+        <h1 className="fs-1">
+          <span className="display-4 fw-semibold darkGolden-color">D</span>
+          etalle de la receta
+        </h1>
+        <hr/>
+        <Row className="justify-content-center align-items-center">
+        <Col md={6} className="mb-4 mb-md-0">
+          <img
+            src="https://images.pexels.com/photos/1351238/pexels-photo-1351238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Imagen del producto"
+            className="img-detalle"
+          />
+        </Col>
+        <Col md={6}>
+          <h2 className="darkGolden-color fw-semibold text-uppercase">Pan con espinaca y queso</h2>
+          <hr />
+          <p>
+            El café americano es una bebida caliente que consiste en un espresso
+            diluido con agua caliente, lo que resulta en una taza de café suave
+            y aromático. Es una opción popular para aquellos que prefieren un
+            café menos intenso que el espresso tradicional. Perfecto para
+            disfrutar en cualquier momento del día.
+          </p>
+          <span></span>
+          <span></span>
+        </Col>
+      </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default DetalleReceta;
