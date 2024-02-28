929ee010889dd5c1b0026a8d66925e8ba9a7ea72
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-02-28 12:28:29 -0300

    Se enlazaron las rutas correspondientes dentro del componente menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,16 +1,18 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
     <Navbar expand="md" className="bg-darkGolden py-3">
       <Container>
-        <Navbar.Brand className="logo"><i className="bi bi-egg-fried me-2"></i>DeliciasVirtuales</Navbar.Brand>
+        <Navbar.Brand className="logo" as={Link} to="/"><i className="bi bi-egg-fried me-2"></i>DeliciasVirtuales</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto textPoppins">
-            <Nav.Link className="text-light">Administrador</Nav.Link>
-            <Nav.Link className="text-light">Iniciar Sesión</Nav.Link>
-            <Nav.Link className="text-light">Registrarse</Nav.Link>
+          <Nav className="ms-auto textPoppins text-success">
+            <NavLink end to="/" className="nav-link">Inicio</NavLink>
+            <NavLink end to="/administrador" className=" nav-link">Administrador</NavLink>
+            <NavLink to="/iniciar-sesion" className=" nav-link">Iniciar Sesión</NavLink>
+            <NavLink to="/registrarse" className=" nav-link">Registrarse</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
